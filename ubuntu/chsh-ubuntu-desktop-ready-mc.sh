58dd774b26e9480136860481d3c1fdd06bcf77fe
Dmitry Antonov <dant@dant.pro>
2025-11-23 02:02:46 +0700

    something was changed

---
+++ ubuntu/chsh-ubuntu-desktop-ready-mc.sh
@@ -0,0 +1,139 @@
+#! /bin/sh
+
+sudo apt install -y mc
+
+sudo apt install -y antiword
+
+sudo apt install -y arc
+sudo apt install -y arcanist
+
+sudo apt install -y arj
+
+sudo apt install -y cabextract
+
+sudo apt install -y catdoc
+
+sudo apt install -y texlive-binaries
+
+sudo apt install -y clzip
+sudo apt install -y lunzip
+sudo apt install -y lzd
+sudo apt install -y lzip
+sudo apt install -y lziprecover
+sudo apt install -y minilzip
+sudo apt install -y pdlzip
+sudo apt install -y plzip
+
+sudo apt install -y ctorrent
+
+sudo apt install -y dbview
+
+sudo apt install -y default-jdk-headless
+
+sudo apt install -y djview4
+
+sudo apt install -y djvulibre-bin 
+
+sudo apt install -y elinks
+
+sudo apt install -y epub-utils
+sudo apt install -y ncbi-entrez-direct
+
+sudo apt install -y exif
+
+sudo apt install -y genisoimage
+
+sudo apt install -y gettext
+
+sudo apt install -y glade
+
+sudo apt install -y gputils
+
+sudo apt install -y gv
+
+sudo apt install -y imagemagick
+
+sudo apt install -y jlha-utils
+sudo apt install -y lhasa
+
+sudo apt install -y kchmviewer
+
+sudo apt install -y libaspell-dev 
+
+sudo apt install -y libbatik-java 
+
+sudo apt install -y libchm-bin 
+
+# sudo apt install -y liblz4-tool 
+
+sudo apt install -y libxml2-utils
+
+sudo apt install -y links2
+
+sudo apt install -y links
+sudo apt install -y w3m
+lynx
+
+sudo apt install -y mikmod
+
+sudo apt install -y mpg321
+
+sudo apt install -y mplayer
+
+sudo apt install -y odt2txt
+sudo apt install -y unoconv
+
+sudo apt install -y p7zip-full
+
+sudo apt install -y par2
+
+sudo apt install -y poedit
+sudo apt install -y potool
+
+sudo apt install -y procyon-decompiler 
+
+sudo apt install -y python3-tz
+
+sudo apt install -y rar
+
+sudo apt install -y rpm
+
+sudo apt install -y sox
+
+sudo apt install -y sqlite3
+
+sudo apt install -y timidity
+
+sudo apt install -y unace
+sudo apt install -y unace-nonfree
+
+sudo apt install -y unalz
+
+sudo apt install -y unar
+
+sudo apt install -y unrar
+sudo apt install -y unrar-free
+
+sudo apt install -y vorbis-tools
+
+sudo apt install -y wimtools
+
+sudo apt install -y wv
+
+sudo apt install -y xpmutils
+
+
+#  antiword          | minilzip            genisoimage    liblz4-tool    p7zip-full          unace
+#  arc               | pdlzip              gettext        libxml2-utils  par2                | unace-nonfree
+#  | arcanist        | plzip               glade          links2         poedit              unalz
+#  arj               ctorrent              gputils        links          | potool            unar
+#  cabextract        dbview                gv             | w3m          procyon-decompiler  unrar
+#  catdoc            default-jdk-headless  imagemagick    | lynx         python3-boto        | unrar-free
+#  texlive-binaries  djview4               jlha-utils     lyx            python3-tz          vorbis-tools
+#  clzip             djvulibre-bin         | lhasa        mikmod         rar                 wimtools
+#  | lunzip          elinks                kchmviewer     mpg321         rpm                 wv
+#  | lzd             epub-utils            libaspell-dev  mplayer        sox                 xpmutils
+#  | lzip            | ncbi-entrez-direct  libbatik-java  odt2txt        sqlite3
+#  | lziprecover     exif                  libchm-bin     | unoconv      timidity
+
+

391a9289c298ae0609803ac9e40be2810135503d
Dmitry Antonov <dant@dant.pro>
2025-12-02 16:11:21 +0700

    251202-161120 should work I guess...

--- commit.sh
+++ commit.sh
@@ -5,7 +5,8 @@ comment=$(curl -L -s http://whatthecommit.com/index.txt)
 
 git add .
 
-echo git push on "${date}" "${comment}"
+# echo git push on "${date}" "${comment}"
+echo -e "git push on ${date} ${comment}\n---"
 
 # for i in $(git status | grep deleted | awk '{print $2}'); do git rm "$i"; done
 
